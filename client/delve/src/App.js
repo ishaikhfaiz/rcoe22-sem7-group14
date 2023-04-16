870ab6e7bd79e7e20fc321bccc0a6ccd09d111e7
Faiz Shaikh <ishaikfaiz@eng.rizvi.edu.in>
2023-04-16 12:36:14 +0530

    Conflict Resolved

--- client/delve/src/App.js
+++ client/delve/src/App.js
@@ -10,7 +10,6 @@ import PostDetails from './components/PostDetails/PostDetails';
 import Auth from './components/Auth/Auth';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Itinerary from './components/Itinerary/Itinerary';
-import Deals from './components/Deals/Deals';
 
 const App = () => {
   const theme = createTheme();
@@ -30,8 +29,7 @@ const App = () => {
               <Route path="/posts/search" exact element={<Home />} />
               <Route path="/posts/:id" element={<PostDetails />} />
               <Route path="/auth" exact element={(!user ? <Auth /> : <Navigate replace to="/posts" />)} />
-              <Route path="/itinerary" exact element={<Itinerary/>} />
-              <Route path="/deals" element={<Deals />} />
+              <Route path="/itinerary" exact element={<Itinerary/>}/>
             </Routes>
 
           </Container>
